f38705710b97ed63f8e69b9d301bd3267fe42acf
Fran Bosquet <frbosquet@gmail.com>
2020-01-20 18:50:18 +0100

    Add scripts

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+  private new Rigidbody rigidbody;
+
+  void Start()
+  {
+    rigidbody = gameObject.GetComponent<Rigidbody>();
+    rigidbody.AddForce(10, 10, 0, ForceMode.VelocityChange);
+  }
+
+  void Update()
+  {
+  }
+}
