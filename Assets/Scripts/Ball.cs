eb384f0f9340a3b0db0e855a1c401cc532be881b
Fran Bosquet <frbosquet@gmail.com>
2020-01-21 19:20:42 +0100

    Add sound for regular bounces

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
   private new Rigidbody rigidbody;
+  private AudioSource kickSound;
 
   void Start()
   {
     rigidbody = gameObject.GetComponent<Rigidbody>();
     rigidbody.AddForce(4, 4, 0, ForceMode.VelocityChange);
+    kickSound = GetComponent<AudioSource>();
   }
 
   private void OnTriggerEnter(Collider other)
@@ -25,5 +27,11 @@ public class Ball : MonoBehaviour
       Brick brick = other.gameObject.GetComponent<Brick>();
       brick.Hit();
     }
+    else if (other.gameObject.CompareTag("Wall"))
+    {
+      kickSound.Play();
+    }
+
+
   }
 }
