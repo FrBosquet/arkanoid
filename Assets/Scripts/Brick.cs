bc8b3fd5c5be4c792067bb80dd7d418ed0b64261
Fran Bosquet <frbosquet@gmail.com>
2020-01-21 19:10:29 +0100

    Add brick explosion particle effect

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Brick : MonoBehaviour
 {
   [SerializeField] private int life = 1;
+  public GameObject particlePrefab;
 
   public void Hit()
   {
@@ -12,6 +13,11 @@ public class Brick : MonoBehaviour
 
     if (life == 0)
     {
+      GameObject particles = Instantiate(particlePrefab, transform.position, Quaternion.identity);
+      ParticleSystem particleSystem = particles.GetComponent<ParticleSystem>();
+
+      particleSystem.Play();
+
       Destroy(gameObject);
     }
   }
