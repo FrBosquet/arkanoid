4e7860526fa7e7ec4b8630c81877fad667f51cf6
Fran Bosquet <frbosquet@gmail.com>
2020-01-22 09:09:39 +0100

    Add level manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+  public List<GameObject> levels;
+  private GameObject level;
+  private int levelIndex = 0;
+
+  private void Start()
+  {
+    Restart();
+  }
+
+  public void Restart()
+  {
+    levelIndex = 0;
+    LoadLevel(0);
+  }
+
+  private void LoadLevel(int index)
+  {
+    Destroy(level);
+    level = Instantiate(levels[index], Vector3.zero, Quaternion.identity);
+  }
+}
