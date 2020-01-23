f8afa5d05ec5bba3f3a00bb6d8778e2410e98e3b
Fran Bosquet <frbosquet@gmail.com>
2020-01-23 10:30:13 +0100

    Refactor time management into its own component

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+  public void SlowDown()
+  {
+    StartCoroutine(SlowDownCoroutine());
+  }
+
+  public void Accelerate()
+  {
+    StartCoroutine(AccelerateCoroutine());
+  }
+
+  IEnumerator SlowDownCoroutine()
+  {
+    while (Time.timeScale > 0.2f)
+    {
+      yield return new WaitForSeconds(0.01f);
+      Time.timeScale -= 0.05f;
+    }
+
+    Time.timeScale = 0;
+  }
+
+  IEnumerator AccelerateCoroutine()
+  {
+    Time.timeScale = 0.2f;
+    while (Time.timeScale < 0.98)
+    {
+      yield return new WaitForSeconds(0.01f);
+      Time.timeScale += 0.05f;
+    }
+
+    Time.timeScale = 1;
+  }
+}
